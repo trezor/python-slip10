2b492d44ad3c206c801f16b2373e2f2beb1a9aed
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2026-01-28 13:26:41 +0100

    fixup! Test `add_points()`

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,13 +5,17 @@ from slip10.utils import SECP256K1, SECP256R1
 
 @pytest.mark.parametrize("curve", (SECP256K1, SECP256R1))
 def test_curve_arithmetic(curve):
-    generator = curve.privkey_to_pubkey((1).to_bytes(32, "big"))
-    minus_generator = curve.privkey_to_pubkey(
-        (curve.curve.group_order - 1).to_bytes(32, "big")
-    )
-    double_generator = curve.privkey_to_pubkey((2).to_bytes(32, "big"))
-    triple_generator = curve.privkey_to_pubkey((3).to_bytes(32, "big"))
+    generator = curve.multiply_generator(1)
+    minus_generator = curve.multiply_generator(curve.order - 1)
+    double_generator = curve.multiply_generator(2)
+    triple_generator = curve.multiply_generator(3)
 
+    assert curve.add_points(bytes.fromhex("00"), generator) == generator
+    assert curve.add_points(generator, bytes.fromhex("00")) == generator
     assert curve.add_points(generator, minus_generator) == bytes.fromhex("00")
     assert curve.add_points(generator, generator) == double_generator
     assert curve.add_points(generator, double_generator) == triple_generator
+
+    assert curve.multiply_generator(0) == bytes.fromhex("00")
+    assert curve.multiply_generator(curve.order) == bytes.fromhex("00")
+    assert curve.multiply_generator(curve.order + 1) == generator
